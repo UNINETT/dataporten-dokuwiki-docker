59639d4740d1522c5ed066da3c8c39c16d69aefe
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-07-04 16:47:38 +0200

    Added echoes

--- start.sh
+++ start.sh
@@ -8,23 +8,22 @@ set -e
 : ${DOKUWIKI_TITLE:?"Need to set DOKUWIKI_TITLE non-empty"}
 : ${DOKUWIKI_LICENSE:?"Need to set DOKUWIKI_LICENSE non-empty"}
 
+echo "Addings ettings to local.php"
+
 sed -i '/dataporten-key/c\\$conf["plugin"]["oauth"]["dataporten-key"] = "'${DATAPORTEN_CLIENTID}'";' /var/www/conf/local.php
 sed -i '/dataporten-secret/c\\$conf["plugin"]["oauth"]["dataporten-secret"] = "'${DATAPORTEN_CLIENTSECRET}'";' /var/www/conf/local.php
 sed -i '/license/c\\$conf["license"] = "'${DOKUWIKI_LICENSE}'";' /var/www/conf/local.php
 sed -i '/title/c\\$conf["title"] = "'${DOKUWIKI_TITLE}'";' /var/www/conf/local.php
 sed -i "/superuser/c\$conf['superuser'] = '@${DOKUWIKI_SUPERUSER}';" /var/www/conf/local.php
 
-#sed -i 's/{REPLACE_ID}/'${DATAPORTEN_CLIENTID}'/g' /var/www/conf/local.php
-#sed -i 's/{REPLACE_SECRET}/'${DATAPORTEN_CLIENTSECRET}'/g' /var/www/conf/local.php
-#sed -i 's/{REPLACE_SUPERUSER}/'${DOKUWIKI_SUPERUSER}'/g' /var/www/conf/local.php
-#sed -i 's/{REPLACE_TITLE}/'${DOKUWIKI_TITLE}'/g' /var/www/conf/local.php
-#sed -i 's/{REPLACE_LICENSE}/'${DOKUWIKI_LICENSE}'/g' /var/www/conf/local.php
-
 if [ ! -f /var/dokuwiki-storage/conf/user.auth.php ]; then
+    echo "user.auth.php not found, initiating"
     mv /users.auth.php /var/dokuwiki-storage/conf/users.auth.php
 fi
 
 if [ ! -f /var/www/conf/acl.auth.php ]; then
+  echo "acl.auth.php not found, initating groups"
+  
   touch /var/www/conf/acl.auth.php
   chmod 777 /var/www/conf/acl.auth.php
 
